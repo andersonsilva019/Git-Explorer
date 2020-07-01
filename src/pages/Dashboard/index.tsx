9e2dd66113724f9190c97fb3e2acf0aa775a495c
Anderson0017 <andeerson0017@gmail.com>
2020-07-01 16:17:49 -0300

    Estilos globais

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-// import { Container } from './styles';
+/* import { Container } from './styles'; */
 
-const Dashboard: React.FC = () => ( 
+const Dashboard: React.FC = () => (
   <h1>Dashboard</h1>
 );
 
