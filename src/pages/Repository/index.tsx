cbc54e5c357351bfb251ee133abf31f1456f38fa
Anderson0017 <andeerson0017@gmail.com>
2020-07-13 20:07:43 -0300

    estilizando detalhe

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 /* Pegando os parametros da rota */
-import { useRouteMatch } from 'react-router-dom';
-// import { Container } from './styles';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
+import imgLogo from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -11,7 +14,54 @@ const Repository: React.FC = () => {
 
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={imgLogo} alt="GitHubExplorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+        Voltar
+      </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descriptim</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1808</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link
+          to={"sadasasd"}
+        >
+          <div>
+            <strong>sadas</strong>
+            <p>dsadasd</p>
+          </div>
+
+          <FiChevronRight size={20} color="#cbcbd6" />
+        </Link>
+      </Issues>
+    </>
+  );
 }
 
 export default Repository;
